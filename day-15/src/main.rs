3e141bf62a018c78896745817e158ade3c894847
Arthur Rosa <arthur@rosafamily.net>
2024-12-15 11:26:05 -0800

    Day 15 part 2

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,26 +1,52 @@
 mod object;
 
+use crate::object::{AddMove, Direction, Object, ObjectType};
+use itertools::Itertools;
 use std::collections::HashMap;
 use utils::{read_file, read_lines, time};
-use crate::object::{Direction, Object, ObjectType};
 
 fn main() {
-    let (part1, time1) = time(|| run_sim("src/map.txt", "src/moves.txt"));
+    let (part1, time1) = time(|| run_sim("src/map.txt", "src/moves.txt", false));
     println!("Part 1: {} (took {} seconds)", part1, time1.as_secs_f64());
+    let (part2, time2) = time(|| run_sim("src/map.txt", "src/moves.txt", true));
+    println!("Part 2: {} (took {} seconds)", part2, time2.as_secs_f64());
 }
 
-fn parse_map(filename: &str) -> (HashMap<(i32, i32), Object>, (i32, i32), i32, i32) {
-    let file = read_lines(filename);
+fn parse_map(filename: &str, wide: bool) -> (HashMap<(i32, i32), Object>, (i32, i32), i32, i32) {
+    let mut file = read_lines(filename);
     let mut map: HashMap<(i32, i32), Object> = HashMap::new();
     let mut robot: (i32, i32) = (-1, -1);
 
-    for (row, line) in file.iter().enumerate() {
-        for (col, c) in line.chars().enumerate() {
-            if c == '#' || c == 'O' {
-                let o = Object::new(c);
-                map.insert((row as i32, col as i32), o);
-            } else if c == '@' {
-                robot = (row as i32, col as i32);
+    if !wide {
+        for (row, line) in file.iter().enumerate() {
+            for (col, c) in line.chars().enumerate() {
+                if c == '#' || c == 'O' {
+                    let o = Object::new(c);
+                    map.insert((row as i32, col as i32), o);
+                } else if c == '@' {
+                    robot = (row as i32, col as i32);
+                }
+            }
+        }
+    } else {
+        file = file
+            .iter()
+            .map(|l| {
+                l.replace("#", "##")
+                    .replace(".", "..")
+                    .replace("@", "@.")
+                    .replace("O", "[]")
+            })
+            .collect_vec();
+        for (row, line) in file.iter().enumerate() {
+            for (col, c) in line.chars().enumerate() {
+                match c {
+                    '[' | '#' | ']' => {
+                        map.insert((row as i32, col as i32), Object::new(c));
+                    }
+                    '@' => robot = (row as i32, col as i32),
+                    _ => (),
+                }
             }
         }
     }
@@ -40,26 +66,20 @@ fn parse_moves(filename: &str) -> Vec<Direction> {
         .collect()
 }
 
-fn run_sim(map_file: &str, moves_file: &str) -> i32 {
-    let (mut map, mut robot, rows, cols) = parse_map(map_file);
+fn run_sim(map_file: &str, moves_file: &str, wide: bool) -> i32 {
+    let (mut map, mut robot, rows, cols) = parse_map(map_file, wide);
     let moves = parse_moves(moves_file);
     for m in moves {
-        let mov = match m {
-            Direction::North => (-1, 0),
-            Direction::South => (1, 0),
-            Direction::East => (0, 1),
-            Direction::West => (0, -1),
-        };
+        let mov = m.to_move();
         let next_position = (robot.0 + mov.0, robot.1 + mov.1);
         if let Some(next) = map.get(&next_position) {
             // Something is in the next position, let's see if we can push it
-            let (new_map, push_result) = next.try_push(m, next_position, map.clone());
+            let (new_map, push_result) = next.try_push(m, next_position, map.clone(), false);
             if push_result {
                 // We pushed it
                 map = new_map;
                 robot = next_position;
             }
-            // No push, no change
         } else {
             // Next position is empty, we can move and the map doesn't change
             robot = next_position;
@@ -71,7 +91,7 @@ fn run_sim(map_file: &str, moves_file: &str) -> i32 {
     for row in 0..rows {
         for col in 0..cols {
             if let Some(o) = map.get(&(row, col)) {
-                if o.kind == ObjectType::Box {
+                if o.kind == ObjectType::Box || o.kind == ObjectType::BoxLeft {
                     score += 100 * row + col;
                 }
             }
@@ -89,6 +109,8 @@ fn print_map(map: &HashMap<(i32, i32), Object>, rows: i32, cols: i32, robot: (i3
                 match o.kind {
                     ObjectType::Wall => print!("#"),
                     ObjectType::Box => print!("O"),
+                    ObjectType::BoxLeft => print!("["),
+                    ObjectType::BoxRight => print!("]"),
                 }
             } else {
                 print!(".");
@@ -104,12 +126,13 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        let score = run_sim("src/sample-map.txt", "src/sample-moves.txt");
+        let score = run_sim("src/sample-map.txt", "src/sample-moves.txt", false);
         assert_eq!(score, 10092);
     }
 
     #[test]
     fn test_part_2() {
-        assert_eq!(true, true);
+        let score = run_sim("src/sample-map.txt", "src/sample-moves.txt", true);
+        assert_eq!(score, 9021);
     }
 }
--- day-15/src/object.rs
+++ day-15/src/object.rs
@@ -8,9 +8,33 @@ pub enum Direction {
     West,
 }
 
+impl Direction {
+    pub fn to_move(&self) -> (i32, i32) {
+        match self {
+            Direction::North => (-1, 0),
+            Direction::South => (1, 0),
+            Direction::East => (0, 1),
+            Direction::West => (0, -1),
+        }
+    }
+}
+
+pub trait AddMove {
+    fn add(self, other: Direction) -> (i32, i32);
+}
+
+impl AddMove for (i32, i32) {
+    fn add(self, other: Direction) -> (i32, i32) {
+        let (x, y) = other.to_move();
+        (self.0 + x, self.1 + y)
+    }
+}
+
 #[derive(Eq, PartialEq, Copy, Clone, Debug)]
 pub enum ObjectType {
     Wall,
+    BoxLeft,
+    BoxRight,
     Box,
 }
 
@@ -25,6 +49,8 @@ impl Object {
             kind: match kind {
                 '#' => ObjectType::Wall,
                 'O' => ObjectType::Box,
+                '[' => ObjectType::BoxLeft,
+                ']' => ObjectType::BoxRight,
                 _ => panic!(),
             },
         }
@@ -35,19 +61,36 @@ impl Object {
         direction: Direction,
         position: (i32, i32),
         map: HashMap<(i32, i32), Object>,
+        from_pair: bool
     ) -> (HashMap<(i32, i32), Object>, bool) {
+        let mut map = map.clone();
         if self.kind == ObjectType::Wall {
             (map, false)
         } else {
+            if !from_pair && (self.kind == ObjectType::BoxLeft || self.kind == ObjectType::BoxRight) &&
+                (direction == Direction::North || direction == Direction::South) {
+                // We're a left or right box so we have to move in tandem with our pair.
+                // This means we just have to only move if our pair can move too.
+                // `from_pair` indicates that we're coming from a pair so we shouldn't
+                // add the one we came from to the check. We only care about N/S moves here
+                // since E/W will be handled correctly by the normal logic
+                let p = match self.kind {
+                    ObjectType::BoxLeft => position.add(Direction::East),
+                    ObjectType::BoxRight => position.add(Direction::West),
+                    _ => unreachable!(),
+                };
+                if let Some(pair) = map.get(&p) {
+                    let (nm, push_result) = pair.try_push(direction, p, map.clone(), true);
+                    if !push_result {
+                        // Pair wasn't able to move, so the map doesn't change and we don't move
+                        return (map, false);
+                    }
+                    map = nm;
+                    // Pair was able to move, continue as normal
+                }
+            }
             // Directions are (row, col)
-            let mov = match direction {
-                Direction::North => (-1, 0),
-                Direction::South => (1, 0),
-                Direction::East => (0, 1),
-                Direction::West => (0, -1),
-            };
-            let row = position.0 + mov.0;
-            let col = position.1 + mov.1;
+            let (row, col) = position.add(direction);
             if let Some(next) = map.get(&(row, col)) {
                 if next.kind == ObjectType::Wall {
                     // Next square is a wall, no moving here
@@ -55,7 +98,7 @@ impl Object {
                 } else {
                     // Next square is another box. Need to see if we can push the next box too
                     let (mut new_map, push_result) =
-                        next.try_push(direction, (row, col), map.clone());
+                        next.try_push(direction, (row, col), map.clone(), false);
                     if push_result {
                         // The next square over moved! We can move too.
                         let o = new_map.remove(&(position.0, position.1)).unwrap();
