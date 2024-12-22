df06a2c9ad7ece0de0dafc448bfd01137b14ac59
Arthur Rosa <arthur@rosafamily.net>
2024-12-21 21:10:19 -0800

    idk anymore

--- day-21/src/main.rs
+++ day-21/src/main.rs
@@ -6,7 +6,38 @@ use crate::moves::Move;
 use itertools::Itertools;
 use memoize::memoize;
 use std::collections::HashMap;
+use std::sync::LazyLock;
 use utils::{time, Point};
+
+static NUMERIC_PAD: LazyLock<HashMap<Button, Point>> = LazyLock::new(|| {
+    vec![
+        (Button::Seven, Point::new(0, 0)),
+        (Button::Eight, Point::new(0, 1)),
+        (Button::Nine, Point::new(0, 2)),
+        (Button::Four, Point::new(1, 0)),
+        (Button::Five, Point::new(1, 1)),
+        (Button::Six, Point::new(1, 2)),
+        (Button::One, Point::new(2, 0)),
+        (Button::Two, Point::new(2, 1)),
+        (Button::Three, Point::new(2, 2)),
+        (Button::Zero, Point::new(3, 1)),
+        (Button::LetterA, Point::new(3, 2)),
+    ]
+    .into_iter()
+    .collect()
+});
+
+static D_PAD: LazyLock<HashMap<Button, Point>> = LazyLock::new(|| {
+    vec![
+        (Button::Up, Point::new(0, 1)),
+        (Button::Down, Point::new(1, 1)),
+        (Button::Left, Point::new(1, 0)),
+        (Button::Right, Point::new(1, 2)),
+        (Button::Activate, Point::new(0, 2)),
+    ]
+    .into_iter()
+    .collect()
+});
 
 fn main() {
     let (part1, time1) = time(|| part_1());
@@ -32,38 +63,14 @@ fn part_2() -> u128 {
 }
 
 fn code_difficulty(desired_code: &str, iterations: usize) -> u128 {
-    let numeric_pad: HashMap<Button, Point> = vec![
-        (Button::Seven, Point::new(0, 0)),
-        (Button::Eight, Point::new(0, 1)),
-        (Button::Nine, Point::new(0, 2)),
-        (Button::Four, Point::new(1, 0)),
-        (Button::Five, Point::new(1, 1)),
-        (Button::Six, Point::new(1, 2)),
-        (Button::One, Point::new(2, 0)),
-        (Button::Two, Point::new(2, 1)),
-        (Button::Three, Point::new(2, 2)),
-        (Button::Zero, Point::new(3, 1)),
-        (Button::LetterA, Point::new(3, 2)),
-    ]
-    .into_iter()
-    .collect();
-    let directional_pad: HashMap<Button, Point> = vec![
-        (Button::Up, Point::new(0, 1)),
-        (Button::Down, Point::new(1, 1)),
-        (Button::Left, Point::new(1, 0)),
-        (Button::Right, Point::new(1, 2)),
-        (Button::Activate, Point::new(0, 2)),
-    ]
-    .into_iter()
-    .collect();
     let mut s = find_control_sequence(
         desired_code,
-        &numeric_pad,
+        &NUMERIC_PAD,
         Button::LetterA,
         &Point::new(3, 0),
     );
     for i in 0..iterations {
-        s = find_control_sequence(&s, &directional_pad, Button::Activate, &Point::new(0, 0));
+        s = find_control_sequence(&s, &D_PAD, Button::Activate, &Point::new(0, 0));
         println!("Finished loop {}", i);
     }
     s.chars().count() as u128 * desired_code[0..3].to_string().parse::<usize>().unwrap() as u128
